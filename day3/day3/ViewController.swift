b69fb0d58c95cea3140156928e7c41a7840b7785
tianliang <tianliang_black@163.com>
2016-05-04 00:53:10 +0800

    day3

---
+++ day3/day3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  day3
+//
+//  Created by tian.liang on 16/5/4.
+//  Copyright © 2016年 welkiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
