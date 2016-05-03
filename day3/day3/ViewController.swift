56610c59313b4e65a371dd74e99e0dff015a483f
tianliang <tianliang_black@163.com>
2016-05-04 00:54:18 +0800

    day3

--- day3/day3/ViewController.swift
+++ day3/day3/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import AVFoundation
+import AVKit
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let contentss = [VideoInfo(image: UIImage.init(named: "videoScreenshot01"), content: "我是一"),
+                     VideoInfo(image: UIImage.init(named: "videoScreenshot02"), content: "我是二"),
+                     VideoInfo(image: UIImage.init(named: "videoScreenshot03"), content: "我是三"),
+                     VideoInfo(image: UIImage.init(named: "videoScreenshot04"), content: "我是四"),
+                     VideoInfo(image: UIImage.init(named: "videoScreenshot05"), content: "我是五"),
+                     VideoInfo(image: UIImage.init(named: "videoScreenshot05"), content: "我是六")]
+    
+    lazy var playViewController = AVPlayerViewController()
+    lazy var playerView = AVPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: tableView相关
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 220
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contentss.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! Cell
+        cell.contentLabel.text = contentss[indexPath.row].content
+        cell.contentImageView.image = contentss[indexPath.row].image
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let path = NSBundle.mainBundle().pathForResource("emoji zone", ofType: "mp4")
+        
+        playerView = AVPlayer(URL: NSURL(fileURLWithPath: path!))
+        
+        playViewController.player = playerView
+        
+        self.presentViewController(playViewController, animated: true) {
+            self.playViewController.player?.play()
+        }
 
+    }
+}
 
+struct VideoInfo {
+    let image: UIImage?
+    let content: String?
+}
+
+class Cell: UITableViewCell {
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    @IBOutlet weak var contentLabel: UILabel!
 }
 
