4ca003346dd0490f36a27460a021a152dcf1b338
welkinman <tianliang.black@gmail.com>
2016-05-03 19:47:45 +0800

    init git

---
+++ day1/day1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  day1
+//
+//  Created by tl on 16/5/3.
+//  Copyright © 2016年 welkiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
