d0c56e1823cd0d491b5d84868a68d5815e3b8be2
welkinman <tianliang.black@gmail.com>
2016-05-03 20:49:57 +0800

    finish day 1

--- day1/day1/ViewController.swift
+++ day1/day1/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    dynamic var count = 0.0 {
+        didSet{
+            timeLabel.text = String(count)
+        }
+    }
+    var isPlaying = false
+    var timer: NSTimer?
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.timeLabel.text = String(count)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func beginBtnClick(sender: UIButton) {
+        guard !isPlaying else {
+            return
+        }
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.start), userInfo: nil, repeats: true)
+        isPlaying = true
+    }
+    func start() -> Void {
+        self.count += 0.1
+    }
+    @IBAction func suspendBtnClick(sender: UIButton) {
+        timer?.invalidate()
+        isPlaying = false
+    }
+    
+    @IBAction func resetBtnClick(sender: UIButton) {
+        self.count = 0
+        isPlaying = false
+        timer?.invalidate()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
